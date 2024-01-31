92a3625010497893dae815f12d7e587539ac5489
EhsanRD <ehsan.rezaei.d@gmail.com>
2024-01-31 12:36:00 +1100

    generate design matrix and treatment effect variances for linear and categorical time effects

--- 1. functions_sim.R
+++ 1. functions_sim.R
@@ -1,30 +1,12 @@
 # Codes were taken from https://github.com/klgrantham/compare-SC-SW
-# Generate stepped wedge design matrix
-# with S sequences and K clusters assigned to each sequence
-SWdesmat <- function(S, K=1) {
-  # Inputs:
-  #  S - number of unique treatment sequences
-  #  K - number of times each sequence is repeated
-  #  (S*K = number of clusters)
-  # Output:
-  #  Design matrix
-  Xsw <- matrix(data=0, ncol=(S+1), nrow=S)
-  for(i in 1:S) {
-    Xsw[i,(i+1):(S+1)] <- 1
-  }
-  XswK <- Xsw[sort(rep(1:S, K)), ]
-  return(XswK)
-}
 
-stopifnot(colSums(SWdesmat(3, 1))[4] == 3)
-stopifnot(colSums(SWdesmat(5, 2))[3] == 4)
-# Generate staircase design matrix
+# Generate basic staircase design matrix
 SCdesmat <- function(S, K=1, pre=1, post=1) {
   # Inputs:
-  #  S - number of treatment sequences/clusters
-  #  K - number of times each sequence is repeated
-  #  pre - number of pre-switch measurement periods
-  #  post - number of post-switch measurement periods
+  #  S - number of unique treatment sequences
+  #  K - number of times each sequence is repeated/number of clusters per sequence
+  #  pre - number of pre-switch measurement periods;default set to 1
+  #  post - number of post-switch measurement periods;default set to 1
   # Output:
   #  Design matrix
   Xsc <- matrix(data=NA, nrow=S, ncol=(S+pre+post-1))
@@ -36,61 +18,9 @@ SCdesmat <- function(S, K=1, pre=1, post=1) {
   return(XscK)
 }
 
-# stopifnot(colSums(SCdesmat(3, 2, 1, 1), na.rm=TRUE) == c(0, 2, 2, 2))
-# stopifnot(colSums(SCdesmat(5, 1, 2, 2), na.rm=TRUE)[2] == 0)
-# stopifnot(colSums(SCdesmat(4, 2, 1, 2), na.rm=TRUE)[6] == 2)
+# Calculate staircase treatment effect variance
 
-# Calculate multiple-period CRT treatment effect variance
-CRTvartheta <- function(m, Xmat, rho0, r, corrtype, pereff) {
-  # Inputs:
-  #  m - number of subjects per cluster-period
-  #  Xmat - design matrix (period effects and treatment sequences)
-  #  rho0 - within-period intracluster correlation
-  #  r - cluster autocorrelation
-  #  corrtype - within-cluster correlation structure type
-  #             (0=block-exchangeable, 1=exponential decay)
-  #  pereff - time period effect type
-  #           ('cat'=categorical period effects, 'lin'=linear period effects)
-  # Output:
-  #  Variance of treatment effect estimator
-  # Assumptions:
-  #  Total variance = 1
-  
-  totalvar <- 1
-  sig2CP <- rho0*totalvar
-  sig2E <- totalvar - sig2CP
-  sig2 <- sig2E/m
-  
-  Tp <- ncol(Xmat)
-  K <- nrow(Xmat)
-  Xvec <- as.vector(t(Xmat))
-  
-  if(pereff=='cat'){
-    stackI <- matrix(rep(diag(1,Tp)), nrow=K*Tp, ncol=Tp, byrow=TRUE)
-    Zmat <- cbind(stackI[!is.na(Xvec),], Xvec[!is.na(Xvec)])
-  }else if(pereff=='lin'){
-    stackT <- matrix(1:Tp, nrow=K*Tp, ncol=1, byrow=TRUE)
-    stackT<- cbind(rep(1, nrow(stackT)), stackT)
-    Zmat <- cbind(stackT[!is.na(Xvec),], Xvec[!is.na(Xvec)])
-  }
-  
-  # Covariance matrix for one cluster, with decay in correlation over time
-  if(corrtype==0){
-    # Block-exchangeable structure if corrtype==0
-    Vi <-diag(sig2 +(1-r)*sig2CP, Tp) + matrix(data=sig2CP*r, nrow=Tp, ncol=Tp)
-  }else if(corrtype==1){
-    # Exponential decay structure if corrtype==1
-    Vi <- diag(sig2,Tp) + sig2CP*(r^abs(matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=FALSE) -
-                                          matrix(1:Tp,nrow=Tp, ncol=Tp, byrow=TRUE)))
-  }
-  # Covariance matrix for all K clusters
-  Vall <- kronecker(diag(1,K), Vi)
-  Vall <- Vall[!is.na(Xvec),!is.na(Xvec)]
-  
-  return(solve((t(Zmat)%*%solve(Vall)%*%Zmat))[ncol(Zmat),ncol(Zmat)])
-}
-
-VarSClin <- function(m, S, K, rho0, r){
+VarSClin <- function(S,K,m, rho0, r){
   a <- (1 + (m-1)*rho0)/m
   b <- r*rho0
   
@@ -98,7 +28,7 @@ VarSClin <- function(m, S, K, rho0, r){
   return(vartheta)
 }
 
-VarSCcat <- function(m, S, K, rho0, r){
+VarSCcat <- function(S,K,m, rho0, r){
   a <- (1 + (m-1)*rho0)/m
   b <- r*rho0
   
@@ -107,26 +37,9 @@ VarSCcat <- function(m, S, K, rho0, r){
   return(vartheta)
 }
 
-VarSW_alt <- function(m, S, K, rho0, r){
-  totalvar <- 1
-  sig2CP <- rho0*totalvar
-  sig2E <- totalvar - sig2CP
-  sig2 <- sig2E/m
-  a <- sig2 + sig2CP
-  b <- r*sig2CP
-  (12*(a - b)*(a + S*b))/(K*(S^2 - 1)*(2*a + S*b))
-}
-
 pow <- function(vars, effsize, siglevel=0.05){
   z <- qnorm(siglevel/2)
   pow <- pnorm(z + sqrt(1/vars)*effsize)
   return(pow)
 }
 
-psi_corr <- function(m, rho0, r){
-  (m*rho0*r)/(1+(m-1)*rho0)
-}
-
-implied_r <- function(m, rho0, psi){
-  psi*(1+(m-1)*rho0)/(m*rho0)
-}
