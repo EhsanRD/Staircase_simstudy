c4a16a385e18f22aeb198a81fd35408f7cfaa298
EhsanRD <ehsan.rezaei.d@gmail.com>
2024-02-01 11:37:46 +1100

    run an example and store the results

--- 3. sim.R
+++ 3. sim.R
@@ -7,15 +7,21 @@ library(dplyr)
 
 #sim_res(nsim, S, K, m, ICC, CAC, theta)
 
-reslt <- sim_res(50, 10,5,20, 0.05, 0.8, 0.15)
+pow(VarSCcat(10, 1, 100, 0.01, 0.8),0)
+reslt <- sim_res_fit(1000, 10, 1, 100, 0.01, 0.8,0)
+#reslt <- sim_res_fit(1000, 10, 1, 100, 0.01, 0.95,0)
+
 saveRDS(reslt, file = paste0('results/', 'par_emp_vals', '.RDS'))
 
 vals <- readRDS(file = 'results/par_emp_vals.RDS')
 
 perfs <- vals %>%
-  mutate(Bias_trt_HH=m_est_HH-theta,Bias_trt_BE=m_est_BE-theta,
-                    Bias_ICC_HH=m_estICC_HH-ICC,
-                    Bias_ICC_BE=m_estICC_BE-ICC, 
-                    Bias_CAC_BE=m_estCAC_BE-CAC)
+  mutate(Bias_trt_HH=m_est_trt_HH-theta,Bias_trt_BE=m_est_trt_BE-theta,
+                    Bias_ICC_HH=m_est_ICC_HH-ICC,
+                    Bias_ICC_BE=m_est_ICC_BE-ICC, 
+                    Bias_CAC_BE=m_est_CAC_BE-CAC)
 
 
+
+vals <- readRDS(file = 'results/par_emp_vals.RDS')
+vals2 <- readRDS(file = 'results/par_emp_vals2.RDS')
