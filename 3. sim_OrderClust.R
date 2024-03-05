985e8c655cce7502132e809dba856dc0bd7bc2bc
EhsanRD <ehsan.rezaei.d@gmail.com>
2024-03-05 11:31:46 +1100

    adjust simulation function

--- 3. sim_OrderClust.R
+++ 3. sim_OrderClust.R
@@ -13,13 +13,13 @@ source('2. gd_fit_res.R')
 #run the simulation for one single setting
 
 # Your parameter vectors
-nsim_values <- 1000
-S_values <- c(4,10)
-K_values <-c(1, 5, 10)  
-m_values <- c(10, 50, 100)  
+nsim_values <- 2
+S_values <-c(4,10)
+K_values <- c(1, 5, 10)  
+m_values <- 10#c(10, 50, 100)  
 ICC_values <-c(0.01, 0.05, 0.1, 0.2) 
-CAC_values <- c(1, 0.95, 0.8, 0.5) 
-theta_values <- c(0, 0.15) 
+CAC_values <- 0.5#c(1, 0.95, 0.8, 0.5) 
+theta_values <- 0#c(0, 0.15) 
 type_values <- c("cat","lin")
 
 # Create a list of all combinations of parameter values
@@ -41,28 +41,25 @@ filter_combinations <- function(order) {
 
 # Specify the desired order
 desired_order <- list(
-  c(10, 5, 100),
-  c(4, 10, 100),
-  c(4, 5, 100),
-  c(10, 5, 50),
-  c(10, 1, 100),
-  c(4, 10, 50),
-  c(10, 10, 100),
-  c(4, 5, 50),
-  c(10, 10, 50),
-  c(4, 1, 100),
-  c(10, 1, 50),
+  # c(10, 5, 100),
+  #  c(4, 10, 100),
+  # c(4, 5, 100),
+  # c(10, 5, 50),
+  # c(10, 1, 100),
+  # c(4, 10, 50),
+  # c(10, 10, 100),
+  # c(4, 5, 50),
+  # c(10, 10, 50),
+  # c(4, 1, 100),
+  # c(10, 1, 50),
   c(10, 5, 10),
-  c(4, 1, 50),
+  # c(4, 1, 50),
   c(4, 10, 10),
   c(4, 5, 10),
   c(4, 1, 10),
   c(10, 10, 10),
   c(10, 1, 10)
 )
-#Function to run sim_res_fit for a specific combination of parameters
-#The duration time for each configuration might include the overhead of setting up and managing the parallel processes.
-#Note: the durt is not accurate!
 run_all_sim <- function(params) {
   start_time <- format(Sys.time(), "%a %b %d %X %Y %Z")
   timeStart<-Sys.time()
@@ -70,19 +67,18 @@ run_all_sim <- function(params) {
   end_time <- format(Sys.time(), "%a %b %d %X %Y %Z")
   timeEnd<-Sys.time()
   durt <- as.numeric(difftime(timeEnd, timeStart, units = "mins"))
-  reslt_main<- reslt[[1]]
-  reslt_est <- reslt[[2]]
-  return(list(reslt_main,start_time,end_time,durt,reslt_est))
+  tim_df <- cbind(params$nsim, params$S, params$K, params$m, params$ICC, params$CAC, params$theta,as.character(params$typ),start_time,end_time,durt)
+  return(list(reslt,tim_df))
 }
 # Initialize an empty list to store results
-all_res_lst <- list()
+all_est_lst <- list()
 numCores <- detectCores()
 # Loop through each desired order
 for (gpparams in desired_order) {
   
   #Filter combinations based on the specific order
   filtered_combinations <- filter_combinations(gpparams)
- 
+  
   #Split the filtered combinations into a list of data frames
   sim_lst <- split(filtered_combinations, seq_len(nrow(filtered_combinations)))
   
@@ -101,25 +97,21 @@ for (gpparams in desired_order) {
   clusterExport(clust, c("sim_res_fit", "fitmodels", "gen_dat", "SCdesmat",
                          "fitHHmodelSC", "fitBEmodelSC", "pow" ,"VarSCcat","VarSClin"))
   
-  res_lst <- parLapply(clust, sim_lst, run_all_sim)
+  est_lst <- parLapply(clust, sim_lst, run_all_sim)
   stopCluster(clust)
   
   # Append the results to the list
-  all_res_lst <- c(all_res_lst, res_lst)
+  all_est_lst <- c(all_est_lst, est_lst)
 }
 
+
 # Combine the results into a single data frame
-res_df <- do.call(rbind, lapply(all_res_lst, function(x) x[[1]]))
-resall_df <- res_df %>%
-  mutate(Biastrt_HH = mesttrt_HH - theta, Biastrt_BE = mesttrt_BE - theta,
-         BiasICC_HH = mestICC_HH - ICC, BiasICC_BE = mestICC_BE - ICC,
-         BiasCAC_BE = mestCAC_BE - CAC)
+est_df <- do.call(rbind, lapply(all_est_lst, function(x) x[[1]]))
+#write.csv(est_df, "all_estimates.csv", row.names = FALSE)
 
-resall_df$startmin <- unlist(lapply(all_res_lst, function(x) x[[2]]))
-resall_df$endmin <- unlist(lapply(all_res_lst, function(x) x[[3]]))
-resall_df$durtmin <- unlist(lapply(all_res_lst, function(x) x[[4]]))
-resest_df <- do.call(rbind, lapply(all_res_lst, function(x) x[[5]]))
+sve_t <- do.call(rbind, lapply(all_est_lst, function(x) x[[2]]))
+adj_names <- c("nsim", "S", "K", "m", "ICC", "CAC", "theta", "type","start_time","end_time","durt")
+colnames(sve_t) <- adj_names
+#write.csv(sve_t, "time_records.csv", row.names = FALSE)
 
-#Save the data frames to a csv file
-write.csv(resall_df, "result_overall.csv", row.names = FALSE)
-write.csv(resest_df, "result_indivdiual_estimates.csv", row.names = FALSE)
+
